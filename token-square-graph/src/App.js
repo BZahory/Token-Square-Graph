495f09172547483f99554d91ac02e64e8729c0df
rovilay <rovilayjnr01@gmail.com>
2022-05-12 22:43:51 +0100

    feat: token allocation chart

--- token-square-graph/src/App.js
+++ token-square-graph/src/App.js
@@ -1,20 +1,11 @@
-import logo from './logo.svg';
 import './App.css';
 import React from 'react';
-import ReactDOM from 'react-dom';
-import { VictoryBar } from 'victory';
-
-const data = [
-  {quarter: 1, earnings: 13000},
-  {quarter: 2, earnings: 16500},
-  {quarter: 3, earnings: 14250},
-  {quarter: 4, earnings: 19000}
-];
+import TokenAllocation from './TokenAllocation.jsx';
 
 function App() {
   return (
     <div className="App">
-      <VictoryBar/>
+      <TokenAllocation />
     </div>
   );
 }
