8a492af32c6af4c3fff1aa56e972932604d04feb
Richard Woo <richard.woo@consensys.net>
2022-05-13 09:24:40 -0700

    add color, add cross project by sector

--- token-square-graph/src/App.js
+++ token-square-graph/src/App.js
@@ -2,16 +2,20 @@ import './App.css';
 import React from 'react';
 import TokenAllocation from './TokenAllocation.jsx';
 import { CanvasTotal, CanvasWeekly, CurveOverlay } from './MultiLineGraphs';
-import CrossProjectDashboard from "./CrossProjectDashboard";
+import {
+  CrossProjectDashboard,
+  CrossProjectBySector,
+} from "./CrossProjectDashboard";
 
 function App() {
   return (
     <div className="App">
       <TokenAllocation />
-      <CanvasTotal/>
-      <CanvasWeekly/>
-      <CurveOverlay/>
+      <CanvasTotal />
+      <CanvasWeekly />
+      <CurveOverlay />
       <CrossProjectDashboard />
+      <CrossProjectBySector />
     </div>
   );
 }
--- token-square-graph/src/CrossProjectDashboard.jsx
+++ token-square-graph/src/CrossProjectDashboard.jsx
@@ -5,6 +5,8 @@ import {
   VictoryGroup,
   VictoryTooltip,
   VictoryAxis,
+  VictoryLegend,
+  VictoryTheme,
 } from "victory";
 import { cleanedAllocations } from "./tokenallocation";
 
@@ -21,27 +23,54 @@ function formatData(data) {
   }, []);
 }
 
-const CrossProjectDashboard = () => {
-  const grouped = cleanedAllocations.map((allocation) =>
-    formatData(allocation)
-  );
+const groupBySector = (data) => {
+  const defi = data.filter((o) => o.sector === "DEFI");
+  const nft = data.filter((o) => o.sector === "NFT");
 
-  const g = grouped.slice(0, 4);
+  const average = (category, key) =>
+    category.reduce((acc, curr) => acc + curr[key], 0) / category.length;
 
+  const grouped = [
+    [
+      ...categories.map((cat) => ({
+        x: cat,
+        y: average(defi, cat),
+        fill: "tomato",
+        label: "defi",
+      })),
+    ],
+    [
+      ...categories.map((cat) => ({
+        x: cat,
+        y: average(nft, cat),
+        fill: "orange",
+        label: "nft",
+      })),
+    ],
+  ];
+
+  return grouped;
+};
+
+export const CrossProjectBySector = () => {
+  const grouped = groupBySector(cleanedAllocations);
   return (
     <div className="container">
-      <h1 className="text-4xl">Cross Project Comparison</h1>
+      <h1 className="text-4xl">Cross Project Comparison Average By Sector</h1>
       <div className="col-span-12 flex items-center">
         <VictoryChart
-          domainPadding={50}
+          height={200}
+          width={500}
+          padding={{ top: 50, bottom: 50, left: 75, right: 75 }}
         >
-          <VictoryGroup offset={5}>
-            {/* TODO: fix the scale, add labels */}
+          <VictoryGroup offset={25}>
             {grouped.map((alloc, i) => (
               <VictoryBar
                 key={i}
                 data={alloc}
-                labelComponent={<VictoryTooltip />}
+                style={{ labels: { fontSize: 8 } }}
+                labels={({ datum }) => datum.label}
+                // labelComponent={<VictoryTooltip />}
                 categories={{ x: alloc.x }}
               />
             ))}
@@ -52,4 +81,54 @@ const CrossProjectDashboard = () => {
   );
 };
 
-export default CrossProjectDashboard;
+export const CrossProjectDashboard = () => {
+  const grouped = cleanedAllocations.map((allocation) =>
+    formatData(allocation)
+  );
+
+  return (
+    <div className="container">
+      <h1 className="text-4xl">Cross Project Comparison By Allocation</h1>
+      <div className="col-span-12 flex items-center">
+        <VictoryChart
+          height={400}
+          width={550}
+          padding={{ top: 50, bottom: 50, left: 50, right: 50 }}
+          domainPadding={50}
+          theme={VictoryTheme.material}
+        >
+          <VictoryGroup
+            offset={5}
+            // padding={{ bottom: 50, left: 50, right: 50 }}
+          >
+            {/* <VictoryAxis
+              dependentAxis
+              height={400}
+              offsetY={200}
+              standalone={false}
+            />
+
+            <VictoryAxis
+              // domain={[-10, 10]}
+              standalone={false}
+              tickValues={categories}
+              // offsetY={200}
+            /> */}
+            {grouped.map((alloc, i) => (
+              <VictoryBar
+                key={i}
+                data={alloc}
+                style={{ labels: { fontSize: 6 } }}
+                // labelComponent={<VictoryTooltip />}
+                labels={({ datum }) => datum.label}
+                categories={{ x: alloc.x }}
+              />
+            ))}
+          </VictoryGroup>
+        </VictoryChart>
+      </div>
+    </div>
+  );
+};
+
+// export default CrossProjectDashboard;
